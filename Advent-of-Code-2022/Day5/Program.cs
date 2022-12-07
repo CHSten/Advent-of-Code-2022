30b4accef6ca20b4590ad46c9d52fcb41a2dcaab
Christian Povlsen <chriskp63@gmail.com>
2022-12-07 17:27:04 +0100

    Day 5 part 2 & Day 6

--- Advent-of-Code-2022/Day5/Program.cs
+++ Advent-of-Code-2022/Day5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Advent_of_Code_2022;
 using System.Diagnostics;
+using System.Runtime.Serialization.Formatters.Binary;
 
 namespace Day5
 {
@@ -22,30 +23,65 @@ namespace Day5
             for (int i = 0; i < amountOfCrates; i++)
                 crateStacks.Add(new Stack<string>());
 
-            for(int i = 0; i < crateString.Count; i++)
+            for(int i = crateString.Count - 1; i >= 0; i--)
             {
+                if (string.IsNullOrWhiteSpace(crateString[i]))
+                    continue;
+
                 int idx = i % amountOfCrates;
                 crateStacks[idx].Push(crateString[i]);
             }
+
+            List<Stack<string>> crateStacksNoOrder = DeepCopy(crateStacks);
+            List<Stack<string>> crateStacksOrder = DeepCopy(crateStacks);
 
             foreach (var instruction in instructions)
             {
                 string[] str = instruction.Split(new string[] { "move ", " from ", " to " }, StringSplitOptions.RemoveEmptyEntries);
                 int amountToMove = int.Parse(str[0]);
-
-                List<string> cratesToMove = new List<string>();
+                int fromIdx = int.Parse(str[1]) - 1;
+                int toIdx = int.Parse(str[2]) - 1;
 
                 for (int i = 0; i < amountToMove; i++)
-                    cratesToMove.Add(crateStacks[int.Parse(str[1]) - 1].Pop());
-
-                for (int i = 0; i < cratesToMove.Count; i++)
-                    crateStacks[int.Parse(str[2]) - 1].Push(cratesToMove[i]);
+                    crateStacksNoOrder[toIdx].Push(crateStacksNoOrder[fromIdx].Pop());
             }
 
             Console.WriteLine($"The crates that end at the top of each stack is:\n");
-            foreach (var crate in crateStacks)
+            foreach (var crate in crateStacksNoOrder)
                 Console.WriteLine($"{crate.Peek()}");
 
+            foreach (var instruction in instructions)
+            {
+                string[] str = instruction.Split(new string[] { "move ", " from ", " to " }, StringSplitOptions.RemoveEmptyEntries);
+                int amountToMove = int.Parse(str[0]);
+                int fromIdx = int.Parse(str[1]) - 1;
+                int toIdx = int.Parse(str[2]) - 1;
+                Stack<string> crane = new Stack<string>();
+
+                for (int i = 0; i < amountToMove; i++)
+                    crane.Push(crateStacksOrder[fromIdx].Pop());
+
+                crane.Reverse();
+
+                for (int i = 0; i < amountToMove; i++)
+                    crateStacksOrder[toIdx].Push(crane.Pop());
+            }
+
+            Console.WriteLine($"The crates that end at the top of each stack when order is retained is:\n");
+            foreach (var crate in crateStacksOrder)
+                Console.WriteLine($"{crate.Peek()}");
+
+        }
+
+        public static T DeepCopy<T>(T item)
+        {
+            BinaryFormatter formatter = new BinaryFormatter();
+            MemoryStream stream = new MemoryStream();
+            formatter.Serialize(stream, item);
+            stream.Seek(0, SeekOrigin.Begin);
+            T result = (T)formatter.Deserialize(stream);
+            stream.Close();
+            return result;
         }
     }
 }
---
+++ Advent-of-Code-2022/Day6/Program.cs
@@ -0,0 +1,40 @@
+﻿using Advent_of_Code_2022;
+
+namespace Day6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Input.Get();
+
+            Console.WriteLine($"number of characters processed in marker:\n" +
+                $"{distinctCharacters(input, 4)}");
+
+            Console.WriteLine($"number of characters processed in start-of-message marker:\n" +
+                $"{distinctCharacters(input, 14)}");
+        }
+
+        private static int distinctCharacters(string input, int amountOfDistinctChar)
+        {
+            for (int i = amountOfDistinctChar; i < input.Length; i++)
+            {
+                char[] characters = new char[amountOfDistinctChar];
+                for (int c = 0; c < amountOfDistinctChar; c++)
+                {
+                    int idx = i - c - 1;
+                    characters[c] = input[idx];
+                }
+
+                int distinctElements = characters.Distinct().Count();
+                if (distinctElements == amountOfDistinctChar)
+                {
+                    return i;
+                }
+            }
+
+            throw new Exception();
+
+        }
+    }
+}
