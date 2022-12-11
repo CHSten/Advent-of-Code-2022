5f24bd06d6e013390e030032ede504374444a42b
Christian Povlsen <chriskp63@gmail.com>
2022-12-11 17:53:24 +0100

    Day9

---
+++ Advent-of-Code-2022/Day8/Program.cs
@@ -0,0 +1,160 @@
+﻿using Advent_of_Code_2022;
+using System.Drawing;
+using System.Reflection.Metadata.Ecma335;
+using System.Security.Cryptography.X509Certificates;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace Day8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Input.Get();
+            string[] rows = input.Split("\r\n");
+            string treeArray = string.Join("",rows);
+            int width = rows[0].Length;
+            int height = rows.Length;
+            int[,] tallestTreeLeft = new int[width, height],
+                tallestTreeRight = new int[width, height],
+                tallestTreeUp = new int[width, height],
+                tallestTreeDown = new int[width, height];
+
+            for (int yDown = 0, yUp = height-1; yDown < height && yUp >= 0; yDown++, yUp--)
+            {
+                for (int xLeft = 0, xRight = width-1; xLeft < width && xRight >= 0; xLeft++, xRight--)
+                {
+
+                    //Left
+                    int idx = yDown * width + xLeft;
+                    int tree = int.Parse(treeArray[idx].ToString());
+
+                    if (xLeft - 1 < 0 || tree > tallestTreeLeft[xLeft - 1, yDown])
+                        tallestTreeLeft[xLeft, yDown] = tree;
+                    else
+                        tallestTreeLeft[xLeft, yDown] = tallestTreeLeft[xLeft - 1, yDown];
+
+                    //Right
+                    idx = yDown * width + xRight;
+                    tree = int.Parse(treeArray[idx].ToString());
+
+                    if (xRight+1 > width-1 || tree > tallestTreeRight[xRight + 1, yDown])
+                        tallestTreeRight[xRight, yDown] = tree;
+                    else
+                        tallestTreeRight[xRight, yDown] = tallestTreeRight[xRight + 1, yDown];
+
+                    //Down
+                    idx = yDown * width + xLeft;
+                    tree = int.Parse(treeArray[idx].ToString());
+
+                    if (yDown - 1 < 0 || tree > tallestTreeDown[xLeft, yDown - 1])
+                        tallestTreeDown[xLeft, yDown] = tree;
+                    else
+                        tallestTreeDown[xLeft, yDown] = tallestTreeDown[xLeft, yDown - 1];
+
+                    //Up
+                    idx = yUp * width + xLeft;
+                    tree = int.Parse(treeArray[idx].ToString());
+
+                    if (yUp+1 > height-1 || tree > tallestTreeUp[xLeft, yUp + 1])
+                        tallestTreeUp[xLeft, yUp] = tree;
+                    else
+                        tallestTreeUp[xLeft, yUp] = tallestTreeUp[xLeft, yUp + 1];
+
+                }
+            }
+
+            int visibleTrees = 0;
+            for (int i = 0; i < treeArray.Length; i++)
+            {
+                int tree = int.Parse(treeArray[i].ToString());
+                int x = i % width;
+                int y = i / width;
+
+                if (x == 0 || x == width - 1 ||
+                    y == 0 || y == height - 1)
+                {
+                    visibleTrees++;
+                    continue;
+                }
+
+                if (tallestTreeLeft[x-1, y] < tree ||
+                    tallestTreeRight[x+1, y] < tree ||
+                    tallestTreeDown[x, y-1] < tree ||
+                    tallestTreeUp[x, y+1] < tree)
+                        visibleTrees++;
+            }
+
+            Console.WriteLine($"The number of visible trees:\n" +
+                $"{visibleTrees}");
+
+            int highestScenicScore = 0;
+            for (int i = 0; i < treeArray.Length; i++)
+            {
+                int treeX = i % width,
+                    treeY = i / width;
+
+                if (treeX == 2 && treeY == 3)
+                { }
+
+                int tree = int.Parse(treeArray[i].ToString());
+
+                int leftScore = 0,
+                    rightScore = 0,
+                    downScore = 0,
+                    upScore = 0;
+
+                int x = treeX;
+                do //Left Score
+                {
+                    if (x-1 < 0)
+                        break;
+
+                    x--;
+                    leftScore++;
+                } 
+                while (int.Parse(treeArray[treeY * width + x].ToString()) < tree);
+
+                x = treeX;
+                do //Right Score
+                {
+                    if (x+1 >= width)
+                        break;
+
+                    x++;
+                    rightScore++;
+                } 
+                while (int.Parse(treeArray[treeY * width + x].ToString()) < tree);
+
+                int y = treeY;
+                do //Down Score
+                {
+                    if (y-1 < 0)
+                        break;
+
+                    y--;
+                    downScore++;
+                } 
+                while (int.Parse(treeArray[y * width + treeX].ToString()) < tree);
+
+                y = treeY;
+                do //Up Score
+                {
+                    if (y+1 >= height)
+                        break;
+
+                    y++;
+                    upScore++;
+                } 
+                while (int.Parse(treeArray[y * width + treeX].ToString()) < tree);
+
+                if (highestScenicScore < leftScore * rightScore * downScore * upScore)
+                    highestScenicScore = leftScore * rightScore * downScore * upScore;
+            }
+
+            Console.WriteLine($"Highest possible scenic score:\n" +
+                $"{highestScenicScore}");
+
+        }
+    }
+}
---
+++ Advent-of-Code-2022/Day9/Program.cs
@@ -0,0 +1,217 @@
+﻿using Advent_of_Code_2022;
+using System;
+using System.Windows;
+using System.Runtime.CompilerServices;
+using System.Drawing;
+using System.Diagnostics;
+using System.Numerics;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Day9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var input = Input.Get();
+            //input = "R 5\r\nU 8\r\nL 8\r\nD 3\r\nR 17\r\nD 10\r\nL 25\r\nU 20";
+            var commands = input.Split("\r\n");
+            Point headPosition = new Point(0, 0);
+            Point tailPositionPt1 = new Point(0, 0);
+            List<Point> knotsPt2 = new List<Point>()
+            {
+                new Point(0,0), //1 (0)
+                new Point(0,0), //2 (1)
+                new Point(0,0), //3 (2)
+                new Point(0,0), //4 (3)
+                new Point(0,0), //5 (4)
+                new Point(0,0), //6 (5)
+                new Point(0,0), //7 (6)
+                new Point(0,0), //8 (7)
+                new Point(0,0), //9 (8)
+            };
+            List<Point> visitedPointsPt1 = new List<Point>()
+            {
+                new Point(0, 0),
+            };
+            List<Point> visitedPointsPt2 = new List<Point>()
+            {
+                new Point(0, 0),
+            };
+            List<Vector2> neighbours = new List<Vector2>()
+            {
+                new Vector2(1, 0),
+                new Vector2(-1, 0),
+                new Vector2(0, 1),
+                new Vector2(0, -1),
+                new Vector2(1, 1),
+                new Vector2(-1, -1),
+                new Vector2(-1, 1),
+                new Vector2(1, -1),
+            };
+
+            for (int i = 0; i < commands.Length; i++)
+            {
+                var command = commands[i].Split(" ");
+
+                for (int c = 0; c < int.Parse(command[1]); c++)
+                {
+                    if (command[0] == "R")
+                        headPosition.X++;
+                    else if (command[0] == "L")
+                        headPosition.X--;
+                    else if (command[0] == "U")
+                        headPosition.Y++;
+                    else if (command[0] == "D")
+                        headPosition.Y--;
+
+                    if (headPosition == tailPositionPt1)
+                        continue;
+
+                    Vector2 newPoisition;
+                    Vector2 direction = new Vector2(0, 0);
+                    Vector2 thisDistance = Vector2.Zero;
+                    Vector2? distance = null;
+
+                    foreach (var point in neighbours)
+                    {
+                        newPoisition = new Vector2(tailPositionPt1.X + point.X, tailPositionPt1.Y + point.Y);
+
+                        thisDistance = new Vector2(newPoisition.X - headPosition.X, newPoisition.Y - headPosition.Y);
+
+                        if (thisDistance == Vector2.Zero)
+                            break;
+
+                        if (!distance.HasValue || thisDistance.Length() < distance.Value.Length())
+                        {
+                            direction = point;
+                            distance = thisDistance;
+                        }
+                    }
+
+                    if (thisDistance == Vector2.Zero)
+                        continue;
+
+                    tailPositionPt1.Offset((int)direction.X, (int)direction.Y);
+
+                    if (!visitedPointsPt1.Contains(tailPositionPt1))
+                        visitedPointsPt1.Add(tailPositionPt1);
+                }
+
+            }
+
+            Console.WriteLine($"the number of positions the tail visited atleast once:\n" +
+                $"{visitedPointsPt1.Count}");
+
+            headPosition = new Point(0, 0);
+            for (int i = 0; i < commands.Length; i++)
+            {
+                var command = commands[i].Split(" ");
+
+                for (int c = 0; c < int.Parse(command[1]); c++)
+                {
+                    if (command[0] == "R")
+                        headPosition.X++;
+                    else if (command[0] == "L")
+                        headPosition.X--;
+                    else if (command[0] == "U")
+                        headPosition.Y++;
+                    else if (command[0] == "D")
+                        headPosition.Y--;
+
+                    if (headPosition == knotsPt2[0])
+                        continue;
+
+                    Vector2 newPoisition;
+                    Vector2 direction = new Vector2(0, 0);
+                    Vector2 thisDistance = Vector2.Zero;
+                    Vector2? distance = null;
+                    foreach (var point in neighbours)
+                    {
+                        newPoisition = new Vector2(knotsPt2[0].X + point.X, knotsPt2[0].Y + point.Y);
+
+                        thisDistance = new Vector2(newPoisition.X - headPosition.X, newPoisition.Y - headPosition.Y);
+
+                        if (thisDistance == Vector2.Zero)
+                            break;
+
+                        if (!distance.HasValue || thisDistance.Length() < distance.Value.Length())
+                        {
+                            direction = point;
+                            distance = thisDistance;
+                        }
+                    }
+
+                    if (thisDistance == Vector2.Zero)
+                        continue;
+
+                    knotsPt2[0] = new Point(knotsPt2[0].X + (int)direction.X, knotsPt2[0].Y + (int)direction.Y);
+
+                    for (int k = 1; k < knotsPt2.Count; k++)
+                    {
+                        if (knotsPt2[k] == knotsPt2[k - 1])
+                            continue;
+
+                        newPoisition = new Vector2(0, 0);
+                        direction = new Vector2(0, 0);
+                        thisDistance = Vector2.Zero;
+                        distance = null;
+                        foreach (var point in neighbours)
+                        {
+                            newPoisition = new Vector2(knotsPt2[k].X + point.X, knotsPt2[k].Y + point.Y);
+
+                            thisDistance = new Vector2(newPoisition.X - knotsPt2[k - 1].X, newPoisition.Y - knotsPt2[k - 1].Y);
+
+                            if (thisDistance == Vector2.Zero)
+                                break;
+
+                            if (!distance.HasValue || thisDistance.Length() < distance.Value.Length())
+                            {
+                                direction = point;
+                                distance = thisDistance;
+                            }
+                        }
+
+                        if (thisDistance == Vector2.Zero)
+                            continue;
+
+                        knotsPt2[k] = new Point(knotsPt2[k].X + (int)direction.X, knotsPt2[k].Y + (int)direction.Y);
+                    }
+
+                    if (!visitedPointsPt2.Contains(knotsPt2[knotsPt2.Count-1]))
+                        visitedPointsPt2.Add(knotsPt2[knotsPt2.Count - 1]);
+                }
+            }
+
+            Console.WriteLine($"the number of positions the tail 9 visited atleast once:\n" +
+                $"{visitedPointsPt2.Count}");
+
+            //for (int y = visitedPoints.Max(p => p.Y) + 1; y >= visitedPoints.Min(p => p.Y); y--)
+            //{
+            //    for (int x = visitedPoints.Min(p => p.X); x < visitedPoints.Max(p => p.X) + 1; x++)
+            //    {
+            //        if (x == 0 && y == 0)
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.Yellow;
+            //            Console.Write("s");
+            //            continue;
+            //        }
+
+            //        if (visitedPoints.Contains(new Point(x, y)))
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.Cyan;
+            //            Console.Write("#");
+            //        }
+            //        else
+            //        {
+            //            Console.ForegroundColor = ConsoleColor.White;
+            //            Console.Write(".");
+            //        }
+            //    }
+
+            //    Console.Write("\n");
+            //}
+
+        }
+    }
+}
