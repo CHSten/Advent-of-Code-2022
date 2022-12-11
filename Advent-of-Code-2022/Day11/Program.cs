0ef73a790b05ab5e1a5e1bffa06a1e83131b7fd9
Christian Povlsen <chriskp63@gmail.com>
2022-12-11 21:20:08 +0100

    Working on Day10 part 1

--- Advent-of-Code-2022/Day11/Program.cs
+++ Advent-of-Code-2022/Day11/Program.cs
@@ -1,9 +1,20 @@
 ﻿using Advent_of_Code_2022;
+using System.ComponentModel;
 
 namespace Day11
 {
     internal class Program
     {
+        public class Monkey
+        {
+            public int ID;
+            public List<int> StartingItems = new List<int>();
+            public Action Operation;
+            public Func<bool> Test;
+            public int TrueMonkey;
+            public int FalseMonkey;
+        }
+
         static void Main(string[] args)
         {
             var input = Input.Get();
